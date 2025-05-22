2de9f40c65918677c509a8775ca76545523cfdf2
AmitDasIT <amit@distronix.in>
2025-05-22 12:17:33 +0530

    analytics added

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import Contact from "./layout/contact";
 import Header from "./components/header";
 import { useEffect, useState } from "react";
 import { ArrowUp } from "lucide-react";
+import Gallery from "./layout/gallery";
 
 const App = () => {
   const [isVisible, setIsVisible] = useState(false);
@@ -77,6 +78,10 @@ const App = () => {
         {/* Certifications */}
         <section id="certifications">
           <Certifications />
+        </section>
+
+        <section id="gallery">
+          <Gallery />
         </section>
 
         {/* Contact */}
